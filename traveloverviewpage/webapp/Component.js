d3511000b6e5f8970a87d2217611db7f00d484ee
dlima1 <dimitrilimac@gmail.com>
2021-09-01 00:23:59 +0000

    first commit

---
+++ traveloverviewpage/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/ovp/app/Component'], function(AppComponent) {
+    return AppComponent.extend("sap.fe.demo.traveloverviewpage.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
